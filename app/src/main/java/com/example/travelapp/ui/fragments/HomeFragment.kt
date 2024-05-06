17d4e0faee20513d96849bdbde1a026ccd2f67e6
manishjathanakodi <manishjathanakodi@gmail.com>
2024-05-06 12:14:46 +0530

    implemented navGraph and view binding

--- app/src/main/java/com/example/travelapp/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/travelapp/ui/fragments/HomeFragment.kt
@@ -1,6 +1,32 @@
 package com.example.travelapp.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.travelapp.R
+import com.example.travelapp.databinding.FragmentHomeBinding
 
-class HomeFragment: Fragment() {
+class HomeFragment: BaseFragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private  val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val rv = binding.homeRv
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
